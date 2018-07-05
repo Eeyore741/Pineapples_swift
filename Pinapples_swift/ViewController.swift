22ecc0c79444291dbd01d3bab5b34584c8b13ea9
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2018-07-05 20:47:44 +0500

    1.0.0 - 1

---
+++ Pinapples_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pinapples_swift
+//
+//  Created by Vitaliy Kuznetsov on 05/07/2018.
+//  Copyright © 2018 vitaliikuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
